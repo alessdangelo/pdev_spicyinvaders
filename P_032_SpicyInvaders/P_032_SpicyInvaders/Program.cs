b6bd256751454884c64fde5c2e63025c91be9ae1
alessdangelo <alessandro.dangelo@eduvaud.ch>
2020-10-30 09:41:00 +0100

    Trier directives using

--- P_032_SpicyInvaders/P_032_SpicyInvaders/Program.cs
+++ P_032_SpicyInvaders/P_032_SpicyInvaders/Program.cs
@@ -5,12 +5,8 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Media;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace P_032_SpicyInvaders
 {
