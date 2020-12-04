ce31493605da85c312c63a6f72705ae7c0ab371d
alessdangelo <alessandro.dangelo@eduvaud.ch>
2020-12-04 11:02:39 +0100

    - MoveBullets is now in class Shoot -MoveEnnemies (previously MoveEnnemys) in class Ennemy

--- P_032_SpicyInvaders/P_032_SpicyInvaders/Program.cs
+++ P_032_SpicyInvaders/P_032_SpicyInvaders/Program.cs
@@ -20,17 +20,18 @@ namespace P_032_SpicyInvaders
         /// <summary>
         /// Attributes
         /// </summary>
-        private const int _hudSizeX = 80, _hudSizeY = 50;
+        private const int _windowWidth = 80, _windowHeight = 50;
 
         // Music
+        private static SoundPlayer _music;
         private static DirectSoundOut _soundPlayer = new DirectSoundOut();
         private static ResourceManager _resMan = new ResourceManager(typeof(AppResources.SoundFiles));
-        private static readonly string _mainSong = "song";
+        private static readonly string _mainSong = "Song";
         private static readonly string _shootingEffect = "Laser_Shoot";
         private static readonly string _shotEffect = "Hit_Hurt";
         
         // Objects from class
-        private static Random _random = new Random();
+        private static readonly Random _random = new Random();
         public static Player _ship;
         public static Hud _hud;
         private static Menu _menu;
@@ -54,29 +55,48 @@ namespace P_032_SpicyInvaders
         // Entities arrays and lists
         private static int[] _direction = new int[] { -1, 0 }; //la direction du pack en [x,y]
         private static Enemy[,] _enemiesArray = new Enemy[10, 4]; //10, 4
-        private static int[] _enemiesSpawnPoint = { _hudSizeX / 2 - _enemiesArray.GetLength(0) / 2, _hudSizeY / 2 - 5 - _enemiesArray.GetLength(1) / 2 };
-        private static int[] _enemiesLimits = { 5, _hudSizeX - 5, _enemiesSpawnPoint[1] -3, _enemiesSpawnPoint[1] + 10 }; //les limites du déplacemenmt, en [xMin, xMax, yMin, yMax]
+        private static readonly int[] _enemiesSpawnPoint = { _windowWidth / 2 - _enemiesArray.GetLength(0) / 2, _windowHeight / 2 - 5 - _enemiesArray.GetLength(1) / 2 };
+        private readonly static int[] _enemiesLimits = { 5, _windowWidth - 5, _enemiesSpawnPoint[1] -3, _enemiesSpawnPoint[1] + 10 }; //les limites du déplacemenmt, en [xMin, xMax, yMin, yMax]
         private static List<Block> _blockList = new List<Block>();
         public static List<Shoot> _bullets = new List<Shoot>();
         private static int _ennemyAlive = _enemiesArray.Length;  //Take the numbers of ennemy and decrement it each time one dies.
 
+        // Block Size
+        private static readonly int _blockXSize = 7;
+        private static readonly int _blockYSize = 3;
+
         // State
         private static bool _gamePaused = false;
 
-        // Launch game
-        public static void RunAll()
+        /// <summary>
+        /// Main method: Display Menu
+        /// </summary>
+        static void Main()
         {
             _menu = new Menu();
-            Console.SetWindowSize(50, 50);
+            _menu.MainMenu();
+        }
+
+        /// <summary>
+        /// Run game
+        /// </summary>
+        public static void RunGame()
+        {
+            Console.SetWindowSize(_windowWidth, _windowHeight);
+            Console.SetBufferSize(_windowWidth, _windowHeight);
+
             _ship = new Player(39, 45, 3);
-            _hud = new Hud(_hudSizeX, _hudSizeY);
+            _hud = new Hud();
+            _hud.PrintAllInfos();
 
             // Enable Music
             if (_soundOn)
             {
-                SoundPlayer music = new SoundPlayer();
-                music.Stream = _resMan.GetStream(_mainSong);
-                music.PlayLooping();
+                _music = new SoundPlayer
+                {
+                    Stream = _resMan.GetStream(_mainSong)
+                };
+                _music.PlayLooping();
             }
 
             // Dificulty system
@@ -99,10 +119,10 @@ namespace P_032_SpicyInvaders
             }
 
             // Add blocks
-            _blockList.Add(new Block( 7, 3 , Console.WindowWidth / 4 - 6, 40));
-            _blockList.Add(new Block(7, 3, Console.WindowWidth / 4 + 8, 40));
-            _blockList.Add(new Block(7, 3, Console.WindowWidth / 4 + 24, 40));
-            _blockList.Add(new Block(7, 3, Console.WindowWidth / 4 + 38, 40));
+            _blockList.Add(new Block(_blockXSize, _blockYSize, Console.WindowWidth / 4 - 6, 40));
+            _blockList.Add(new Block(_blockXSize, _blockYSize, Console.WindowWidth / 4 + 8, 40));
+            _blockList.Add(new Block(_blockXSize, _blockYSize, Console.WindowWidth / 4 + 24, 40));
+            _blockList.Add(new Block(_blockXSize, _blockYSize, Console.WindowWidth / 4 + 38, 40));
 
             // init some vars
             ConsoleKeyInfo keyEnterred;
@@ -156,7 +176,15 @@ namespace P_032_SpicyInvaders
 
             }
             while (_gameOver == false);
-            if(_ship.Life < 1)
+
+            WriteHighscore(_highscorePath);
+
+            if (_music != null)
+            {
+                _music.Stop();
+            }
+
+            if (_ship.Life < 1)
             {
                 _menu.GameOver();
             }
@@ -165,17 +193,6 @@ namespace P_032_SpicyInvaders
                 Console.Clear();
                 _menu.Win();
             }
-
-            WriteHighscore(_highscorePath);
-        }
-
-        /// <summary>
-        /// Display Menu
-        /// </summary>
-        static void Main()
-        {
-            _menu = new Menu();
-            _menu.MainMenu();
         }
 
         /// <summary>
@@ -183,72 +200,70 @@ namespace P_032_SpicyInvaders
         /// </summary>
         public static void GlobalMoves()
         {
-                // check is all ennemy are dead
-                foreach (Enemy ennemy in _enemiesArray)
+            // check is all ennemy are dead
+            foreach (Enemy ennemy in _enemiesArray)
+            {
+                if (ennemy.IsAlive == true)
                 {
-                    if (ennemy.IsAlive == true)
-                    {
-                        _gameOver = false;
-                    }
+                    _gameOver = false;
                 }
-
-                Enemy.MoveEnnemies(ref _moveEnnemyAndControlShoot, ref _enemiesSpeed, ref _direction, ref _enemiesArray, _random, ref _bullets, ref _enemiesLimits);
-               Shoot.MoveBullets(ref _bulletMove, ref _bullets);
-                // check if as bullet hit ennemy then detroy ennemy and bullet
-                foreach (Enemy ennemy in _enemiesArray)
-                {
-                    for (int i = 0; i < _bullets.Count; i++)
-                    {
-                        if (_bullets[i] != null && _bullets[i].PosX == ennemy.PosX && _bullets[i].PosY == ennemy.PosY && ennemy.IsAlive)
-                        {
-                            _bullets[i].DestroyBullet();
-                            ennemy.IsAlive = false;
-                            ennemy.DestroyEnemy();
-                            _ennemyAlive--;
-                            GC.Collect();
-                        }
-                    }
-                }
-
-                // check if a bullet hit a block then destroy part of the block
-                foreach (Block block in _blockList)
-                {
-                    for (int i = 0; i < _bullets.Count; i++)
-                    {
-                        if (_bullets[i] != null && block.IsInside(_bullets[i].PosX, _bullets[i].PosY ))
-                        {
-                            _bullets[i].DestroyBullet();
-                            GC.Collect();
-                        }
-                    }
-                }
-
-                // check if a bullet hit the player then decrease lifes
+            }
+            Enemy.MoveEnnemies(ref _moveEnnemyAndControlShoot, ref _enemiesSpeed, ref _direction, ref _enemiesArray, _random, ref _bullets, ref _enemiesLimits);
+            Shoot.MoveBullets(ref _bulletMove, ref _bullets);
+            // check if as bullet hit ennemy then detroy ennemy and bullet
+            foreach (Enemy ennemy in _enemiesArray)
+            {
                 for (int i = 0; i < _bullets.Count; i++)
                 {
-                    if (_bullets[i] != null && _bullets[i].PosX == _ship.PosX && _bullets[i].PosY == _ship.PosY)
+                    if (_bullets[i] != null && _bullets[i].PosX == ennemy.PosX && _bullets[i].PosY == ennemy.PosY && ennemy.IsAlive)
                     {
                         _bullets[i].DestroyBullet();
-                        GC.Collect();
-                        Console.SetCursorPosition(_ship.PosX, _ship.PosY);
-                        Console.Write(_ship.PlayerChar);
-
-                        // invincibility time (when player is hit) & decrement life
-                        if (DateTime.Now > _ship.TempInvicibility)
-                            {
-                                _ship.Invicibility();
-                                PlaySound(_shotEffect);
-                                _ship.Life--;
-                                Hud.PrintPlayerLifes();
-                            }
+                        ennemy.IsAlive = false;
+                        ennemy.DestroyEnemy();
+                        _ennemyAlive--;
                     }
                 }
+            }
 
-                // if player has no more lives or if the ennemies are dead, stop the game and display gameOver
-                if(_ship.Life < 1 || _ennemyAlive == 0)
+            // check if a bullet hit a block then destroy part of the block
+            foreach (Block block in _blockList)
+            {
+                for (int i = 0; i < _bullets.Count; i++)
                 {
-                    _gameOver = true;
+                    if (_bullets[i] != null && block.IsInside(_bullets[i].PosX, _bullets[i].PosY ))
+                    {
+                        _bullets[i].DestroyBullet();
+                    }
                 }
+            }
+
+            // check if a bullet hit the player then decrease lifes
+            for (int i = 0; i < _bullets.Count; i++)
+            {
+                if (_bullets[i] != null && _bullets[i].PosX == _ship.PosX && _bullets[i].PosY == _ship.PosY)
+                {
+                    _bullets[i].DestroyBullet();
+                    Console.SetCursorPosition(_ship.PosX, _ship.PosY);
+                    Console.Write(_ship.PlayerChar);
+
+                    // invincibility time (when player is hit) & decrement life
+                    if (DateTime.Now > _ship.TempInvicibility)
+                        {
+                            _ship.Invicibility();
+                            PlaySound(_shotEffect);
+                            _ship.Life--;
+                            Hud.PrintPlayerLifes();
+                        }
+                }
+            }
+
+            // if player has no more lives or if the ennemies are dead, stop the game and display gameOver
+            if(_ship.Life < 1 || _ennemyAlive == 0)
+            {
+                _gameOver = true;
+            }
+
+            GC.Collect();
         }
         /// <summary>
         /// Play a sound effect
@@ -267,7 +282,7 @@ namespace P_032_SpicyInvaders
         /// Write highscore in txt file
         /// </summary>
         /// <param name="path">txt file path</param>
-        private static void WriteHighscore(string path)
+        public static void WriteHighscore(string path)
         {
             if (File.Exists(path))
             {
